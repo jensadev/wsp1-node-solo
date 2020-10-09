b7c3e9134f4a65bdaa92a9452c2145616693e3b5
Malte Klüft <malte.kluft@gmail.com>
2020-10-09 14:13:00 +0200

    Use a promise-driven asynchronous system for querys

--- routes/story.js
+++ routes/story.js
@@ -1,35 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../models/db');
+const { query } = require('../models/db');
 
-router.get('/:id', function (req, res, next) {
-  let sql = 'SELECT * FROM story WHERE id = ?';
+router.get('/:id', async function (req, res, next) {
+    console.log(req.params);
 
-  console.log(req.params);
-
-  pool.query(sql, [req.params.id], function (err, result, fields) {
-    if (err) throw err;
-
-    sql = 'SELECT * FROM links WHERE story_id = ?';
-
-    let story = result;
-
-    pool.query(sql, [req.params.id], function (err, result, fields) {
-      if (err) throw err;
-      // res.json({
-      //   status: 200,
-      //   id: req.params.id,
-      //   story: story,
-      //   links: result
-      // });
-      res.render('story', { 
-        title: 'Story',
-        id: req.params.id,
-        story: story,
-        links: result
-      });
-    });
-  });
+    try {
+        const story = await query(
+            'SELECT * FROM story WHERE id = ?',
+            req.params.id
+        );
+        
+        const links = await query(
+            'SELECT * FROM links WHERE story_id = ?',
+            req.params.id
+        );
+    
+        res.render('story', {
+            title: 'Story',
+            id: req.params.id,
+            story: story,
+            links: links,
+        });
+    }
+    catch(e) {
+        console.error(e);
+    }
 });
 
 module.exports = router;
